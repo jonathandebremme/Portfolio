a031d00e28c94d53eb94082fdafe075adf212b4b
Jonathan De Bremme <jonathan.debremme@student.hogent.be>
2023-06-25 02:45:46 +0200

    Fixed typo

--- my-portfolio/app/components/LayoutProvider.jsx
+++ my-portfolio/app/components/LayoutProvider.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Navbar from "./NavBar";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export const LayoutProvider = ({ children }) => {
